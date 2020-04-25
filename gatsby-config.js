f0c8fb8728bb3010faad98e2f99b4d032ab68944
Prosper Opara <oparaprosper79@gmail.com>
2020-04-26 00:09:55 +0100

    Chore: enable google font prefetch plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Prosper Opara`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `kodekage`,
+    description: `Prosper Opara portfolio website`,
+    author: `Prosper Opara`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
