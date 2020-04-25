79b7f002dd5f38b02d4827ca956205bfe5306dc8
Prosper Opara <oparaprosper79@gmail.com>
2020-04-25 23:47:44 +0100

    Chore: move work component here since its only used here

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,7 +9,7 @@ import {
 import ScrollReveal from "scrollreveal"
 
 import Layout from "../components/layout"
-import Work from "../components/work"
+// import Work from "../components/work"
 import Project from "../components/projects"
 import Footer from "../components/footer"
 
@@ -41,6 +41,27 @@ class AboutPage extends React.Component {
   }
 
   render() {
+    const Work = ({ img, title, company, detail, duration }) => (
+      <div className="work row justify-content-center align-items-start">
+        <div className="work-company col-lg-3 col-md-4">
+          <img src={img} alt={img} />
+
+          <div>{duration}</div>
+        </div>
+
+        <div className="work-detail col-lg col-md col-s">
+          <div className="work-title">
+            <h3>{company}</h3>
+            <h4>
+              {title} (<i>Remote</i>)
+            </h4>
+          </div>
+
+          <p>{detail}</p>
+        </div>
+      </div>
+    );
+
     return (
       <Layout>
         <div className="about row no-gutters">
