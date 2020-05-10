0fd70a3e66ab877ba66c3650a6bd4862d97af191
Prosper Opara <oparaprosper79@gmail.com>
2020-05-10 17:25:06 +0100

    Add bottom border

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default () => (
   <div
-    style={{ padding: "1em", textAlign: "center", background: "ghostwhite" }}
+    style={{ padding: "1em", textAlign: "center", borderTop: "1px solid rgba(0, 0, 0, 0.1)", color: "#007bff" }}
   >
     Designed and built by Prosper Opara &copy; 2020
   </div>
