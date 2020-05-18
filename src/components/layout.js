e77effe680f17b77a4ddceaf860272f09e3ef85b
Prosper Opara <oparaprosper79@gmail.com>
2020-05-18 10:45:59 +0100

    feature: Add SEO component to improve site SEO

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,24 +7,15 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
 
 import Nav from "./nav"
+import SEO from "./seo"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          short_name
-        }
-      }
-    }
-  `)
-
   return (
     <div className="layout" style={{ overflow: "hidden", maxWidth: "100%" }}>
-      <Nav short_name={data.site.siteMetadata.short_name} />
+      <SEO/>
+      <Nav/>
       {children}
     </div>
   )
