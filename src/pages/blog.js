3063659b6ac471453897a73bc3767efb7169fced
Prosper Opara <oparaprosper79@gmail.com>
2020-05-25 21:04:35 +0100

    ft: Add blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { graphql } from "gatsby"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faMugHot } from "@fortawesome/free-solid-svg-icons"
+import {faMugHot, faClock } from "@fortawesome/free-solid-svg-icons"
 
 import Layout from "../components/layout"
+import Footer from "../components/footer"
 
 // import '../styles/index.css'
 
@@ -13,26 +14,29 @@ const Blog = ({ data }) => {
       <article className="container blog-container">
         <section className="row align-items-stretch justify-content-start no-gutters">
         {data.allMarkdownRemark.edges.map( ({ node }) => {
-          return <div className="col-lg-5 blog-post" key={node.id}>
-            <img src={node.frontmatter.image} alt="blog hero"/>
-            <div>
-              <h4>{node.frontmatter.title}</h4>
-              <span><FontAwesomeIcon icon={faMugHot}/> {node.timeToRead} min read</span>
-              <p>{node.excerpt}</p>
+          return (
+            <a href={`/blog${node.fields.slug}`} className="col-lg-5 blog-post" key={node.id}>
+                <img src={node.frontmatter.image} alt="blog hero"/>
+                <div>
+                  <h4>{node.frontmatter.title}</h4>
+                  <span className="frontmatter"><FontAwesomeIcon icon={faClock}/> {node.frontmatter.date} | <FontAwesomeIcon icon={faMugHot}/> {node.timeToRead} min read</span>
+                  <p>{node.excerpt}</p>
 
-              {node.frontmatter.tags.split(" ").map( (item, index) => <span className="tags" key={index}>#{item}</span>)}
-            </div>
-          </div>
+                  {node.frontmatter.tags.split(" ").map( (item, index) => <span className="tags" key={index}>#{item}</span>)}
+                </div>
+            </a>
+          )
         })}
         </section>
       </article>
+      <Footer/>
     </Layout>
   )
 }
 
 export const query = graphql`
     query {
-        allMarkdownRemark {
+        allMarkdownRemark (sort: {fields: frontmatter___date, order: DESC}) {
             edges {
                 node {
                     id
@@ -42,8 +46,12 @@ export const query = graphql`
                         image
                         description
                         tags
+                        date(formatString: "MMMM DD, YYYY")
                     }
-                    timeToRead
+                    timeToRead,
+                    fields {
+                        slug
+                    }
                 }
             }
         }
